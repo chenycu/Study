96bdd0d27a6d8faa73e461332bce001072f0598d
cheny_company <chenyou920@gmail.com>
2019-07-10 18:30:13 +0800

    没做完的问题

---
+++ src/chapter_2_5/P_2_5_2_CombinedWord.java
@@ -0,0 +1,15 @@
+package chapter_2_5;
+
+/**
+ * 问题：从标准输入读入一列单词并打印出其中所有由两个单词组成的组合词。
+ *       例如：输入的单词为：after through afterthought。那么输出afterthought。
+ * 分析：1、按升序排序:arr[hi+1]；
+ *       2、找到长度>=a[0].length + a[1].length的索引k。
+ *       3、k
+ */
+public class P_2_5_2_CombinedWord {
+
+    public static void main(String[] args) {
+
+    }
+}
